467d358228c2f6a5f3d58582781e11ab4b9048e1
7vik73 <7vik7547@gmail.com>
2025-07-04 19:35:47 +0530

    refactor: switch from MONGODB_URI to MONGO_URI for MongoDB connection

--- backend/src/config/database.config.ts
+++ backend/src/config/database.config.ts
@@ -3,7 +3,7 @@ import { config } from "./app.config";
 
 const connectDatabase = async () => {
   try {
-    await mongoose.connect(config.MONGODB_URI);
+    await mongoose.connect(config.MONGO_URI);
     console.log("Connected to Mongo database");
   } catch (error) {
     console.log("Error connecting to Mongo database");
